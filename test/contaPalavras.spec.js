d570ac09c1a6e7592faf9d3515faf9e1a78f3600
Marmitt <lmateus.marmitt@gmail.com>
2017-05-16 21:25:10 -0300

    Primeiros commits

--- test/contaPalavras.spec.js
+++ test/contaPalavras.spec.js
@@ -3,11 +3,11 @@ const lab = exports.lab = Lab.script();
 
 const { expect } = require('code');
 
-const { calculaTotalItem } = require('../app/contaPalavras');
+const { contaPalavras } = require('../app/contaPalavras');
 
 lab.experiment('Teste com 5 frases', () => {
   lab.test('Deve retornar 5', (done) => {
-  expect(calculaTotalItem("Meu nome é Mateus Marmitt")).to.equal({valor: 5 });
+  expect(contaPalavras("Meu nome é Mateus Marmitt")).to.equal({valor: 5 });
   done();
   });
 });
